d59b6d43f13b632e292454ae7ef57e8d7f6cd30a
Trym <trymj@stud.ntnu.no>
2023-08-27 00:01:29 +0200

    hotfixed navbar to swap between link and scroll link

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './variables.css';
 
 import {Footer, Navbar} from './common/';
@@ -11,18 +11,19 @@ import Home from './home/home';
 import PageNotFound from './pagenotfound/pagenotfound';
 
 function App() {
+
   return (
     <Router>
+      <Navbar/>
       <div>
-        <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/tilbud' element={<TilbudSide/>}/>
           <Route path='/skjemaSide' element={<SkjemaSide/>}/>
           <Route path='*' element={<PageNotFound/>}/>
         </Routes>
-        <Footer/>
       </div>
+      <Footer/>
     </Router>
   );
 }
--- src/common/navbar/navbar.tsx
+++ src/common/navbar/navbar.tsx
@@ -1,31 +1,47 @@
-import React from 'react';
-import default_logo from '../../assets/images/default_logo.svg';
+import React, { useEffect, useState } from 'react';
 import Tilbudbutton from '../tilbudbutton/tilbudbutton';
 import './navbar.css';
 // @ts-ignore
-import { Link } from 'react-scroll';
+import { Link as ScrollLink } from 'react-scroll';
+import { Link, useLocation } from 'react-router-dom';
 
-const navbar = () => {
+function Navbar() {
+    const currentPage = useLocation().pathname;
+    const [isHomePage, setIsHomePage] = useState(useLocation().pathname === '/');
+
+    useEffect(() => {
+        setIsHomePage(currentPage === '/');
+        window.scrollTo(0, 0)
+    }, [currentPage]);
+
     return (
         <header className="header">
             <div className="navbar">
                 <div className="navbar_logo">
-                    <Link to="hero" spy={true} smooth={true} offset={0} duration={500}>
-                        <p>StrømVett</p>
-                    </Link>
+                    {
+                        isHomePage ? (
+                            <ScrollLink to="hero" spy={true} smooth={true} offset={0} duration={500}>
+                                <p>StrømVett</p>
+                            </ScrollLink>
+                        ) : (
+                            <Link to='/' style={{ textDecoration: 'none' }}>
+                                <p>StrømVett</p>
+                            </Link>
+                        )
+                    }
                 </div>
                 <div className="navbar_menu">
                     <ul>
                         <li className='nav-item'>
-                            <Link to="about" spy={true} smooth={true} duration={500}>Om Oss</Link>
+                            <ScrollLink to="about" spy={true} smooth={true} duration={500}>Om Oss</ScrollLink>
                         </li>
                         <li className='nav-item'>
-                            <Link to="avtaler" spy={true} smooth={true}duration={500}>Strømavtaler</Link>
+                            <ScrollLink to="avtaler" spy={true} smooth={true}duration={500}>Strømavtaler</ScrollLink>
                         </li>
                         <li className='nav-item'>
-                            <Link to="footer" spy={true} smooth={true} duration={500}>Kontakt</Link>
+                            <ScrollLink to="footer" spy={true} smooth={true} duration={500}>Kontakt</ScrollLink>
                         </li>
-                        <li className='nav-item'>
+                        <li className='nav-item' style={{borderBottom: 0,}}>
                             <Tilbudbutton/>
                         </li>
                     </ul>
@@ -35,4 +51,4 @@ const navbar = () => {
     )
 }
 
-export default navbar;
+export default Navbar;
